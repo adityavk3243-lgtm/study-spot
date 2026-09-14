560b3c3f7de20deed1de765886d3401b82fce80e
adityavk3243-lgtm <adityavk3243@gmail.com>
2026-09-15 04:22:50 +0530

    Rename SUPABASE_ANON_KEY to SUPABASE_PUBLISHABLE_KEY

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://nhypimulsokajbdozxwx.supabase.co/rest/v1/"
 
-const SUPABASE_ANON_KEY = "sb_publishable_N02LU-du4BnPxSWnWb388w_pJQzpf6b"
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_N02LU-du4BnPxSWnWb388w_pJQzpf6b"
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
