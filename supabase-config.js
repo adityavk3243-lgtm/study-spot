882598ac224a325a7cfc2b839cb60f4e0cf8cd5e
adityavk3243-lgtm <adityavk3243@gmail.com>
2026-09-15 11:13:56 +0530

    Update Supabase URL to remove endpoint path

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://nhypimulsokajbdozxwx.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://nhypimulsokajbdozxwx.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_N02LU-du4BnPxSWnWb388w_pJQzpf6b";
 
